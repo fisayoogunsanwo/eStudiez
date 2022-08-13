7f9ff341e0700262d3bdc925e6b228a51f2115d8
Fisayo Ogunsanwo <90068822+fisayoogunsanwo@users.noreply.github.com>
2022-08-13 03:29:13 -0700

    Update index.js
    index.js updated

--- index.js
+++ index.js
@@ -10,13 +10,19 @@ app.config(function($routeProvider){
         })
         .when("/Teacher",{
             templateUrl: "Teacher_page.html",
+            controller: "myCtrl"
         })
         .when("/Student",{
             templateUrl: "student.html",
+           controller: "myCtrl"
         })
         .when("/Parent_Guardian",{
             templateUrl: "Parent_Guadian.html",
-        })
+            controller: "myCtrl"
+        }).
+      otherwise({
+		  templateUrl:"Welcome.html",
+	});
 })
 
 app.controller('myCtrl',function($scope,$location){
@@ -26,28 +32,29 @@ app.controller('myCtrl',function($scope,$location){
     }
     $scope.shows = function (){
     let name = $scope.name;
+    $scope.infors = "";
     
-    
-        if($scope.same == "Teacher"){
+        if($scope.same == "Teacher" && name != ""){
             sessionStorage.setItem("key",name);
             $scope.name = sessionStorage.getItem("key")
             $location.path("/Teacher")
             welcome.innerHTML = "Hello, Teacher "+name+"!";
         }
-        else if($scope.same == "Student"){
+        else if($scope.same == "Student" && name != ""){
             
             sessionStorage.setItem("key",name);
             $scope.name = sessionStorage.getItem("key")
             $location.path("/Student")
             welcome.innerHTML = "Hello, Student "+name+"!";
         }
-        else if($scope.same == "Parent_Guardian"){
+        else if($scope.same == "Parent_Guardian" && name != ""){
             sessionStorage.setItem("key",name);
             $scope.name = sessionStorage.getItem("key");
             $location.path("/Parent_Guardian")
             welcome.innerHTML = "Hello,  "+name+"!";
         }
         else{
+            $scope.infors = "Please enter your name and select your category";
             sessionStorage.setItem("key","");
             $scope.name = sessionStorage.getItem("key");
             $location.path("/")
